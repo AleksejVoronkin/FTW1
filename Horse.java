1202e8ddbceae77e97aa2e540971c53c83704cd4
AleksejVoronkin <voronkin_aleksej@mail.ru>
2023-12-08 23:56:44 +0300

    добавлены все  животные

---
+++ Horse.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+import java.util.List;
+
+public class Horse extends PackAnimals {
+    public Horse(String name, LocalDate birthDate, List<String> commands) {
+        super(name, birthDate, commands);
+    }
+}
